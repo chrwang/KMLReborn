55f27652df96b9cf1dc70196a21a71e3219fe1a7
Chris Wang <wangch00@icloud.com>
2017-12-18 20:33:51 -0500

    Merge calculations, begin UI integration
    Signed-off-by: Chris Wang <wangch00@icloud.com>

--- src/main/java/Prof/Komo/MainUI.java
+++ src/main/java/Prof/Komo/MainUI.java
@@ -3,16 +3,27 @@ package Prof.Komo;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import org.geotools.data.FileDataStore;
+import org.geotools.data.FileDataStoreFinder;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.styling.SLD;
+import org.geotools.styling.Style;
+import org.geotools.swing.JMapFrame;
+import org.geotools.swing.data.JFileDataStoreChooser;
 
 import javafx.scene.control.TextArea;
 
@@ -68,11 +79,17 @@ import com.vividsolutions.jts.geom.Polygon;
 import java.io.File;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.geotools.swing.data.JFileDataStoreChooser;
 
 /**
- * App front end
+ * App front end, using the one and only JavaFX (and some Swing, because Geotools.)
+ *
+ * Acknowledgements to Professor Andrew R. M. Komo for technical, moral, and metaphorical support
  * Sponsored by Seimens Foundation.
  */
 public class MainUI extends Application
@@ -109,6 +126,9 @@ public class MainUI extends Application
     static double Schwartz = -1;
 
     static TextArea textArea;
+    private File rawMap;
+    private MapContent mc;
+    private Text logView = new Text("Begin KomoLog V0.0.1a");
     
     public static void main( String[] args )
     {
@@ -116,101 +136,89 @@ public class MainUI extends Application
     }
 
     @Override
-    public void start(Stage ps) throws Exception {
+    public void start(Stage ps) {
+        logView.setId("logV");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         ps.setTitle("Prof. Andrew R. M. Komo, Inc.");
 
 
-
         GridPane gd = new GridPane();
-        gd.setAlignment(Pos.CENTER);
+        gd.setAlignment(Pos.TOP_CENTER);
         gd.setHgap(10);
         gd.setVgap(10);
         gd.setPadding(new Insets(25, 25, 25, 25));
 
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(24);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(25);
+        ColumnConstraints col3 = new ColumnConstraints();
+        col3.setPercentWidth(25);
+        ColumnConstraints col4 = new ColumnConstraints();
+        col3.setPercentWidth(25);
+        gd.getColumnConstraints().addAll(col1, col2, col3, col4);
+
         Text scTitle = new Text("Welcome to Prof. Andrew R. M. Komo, Inc (R) Redistricting Maths App!");
-        gd.add(scTitle, 0,0,3,1);
+        scTitle.setId("mainText");
+        scTitle.setTextAlignment(TextAlignment.CENTER);
+        VBox tv = new VBox();
+        tv.getChildren().addAll(scTitle);
+        tv.setAlignment(Pos.TOP_CENTER);
+        gd.add(tv, 0, 0, 4, 1);
 
         Button btn = new Button("Exit");
         btn.setOnAction(event -> System.exit(0));
         HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.setAlignment(Pos.TOP_LEFT);
         hbBtn.getChildren().add(btn);
-        gd.add(hbBtn, 0, 4);
+        gd.add(hbBtn, 0, 1);
 
-        Button b2 = new Button("Start");
+        Button b6 = new Button("Calculate Data");
+        b6.setOnAction(event -> calculate());
+        HBox hb6 = new HBox(10);
+        hb6.setAlignment(Pos.TOP_CENTER);
+        hb6.getChildren().add(b6);
+        gd.add(hb6, 2, 1);
+
+        Button b2 = new Button("Load File");
+        b2.setOnAction(event -> loadMap());
         HBox hb2 = new HBox(10);
-        hb2.setAlignment(Pos.BOTTOM_RIGHT);
+        hb2.setAlignment(Pos.TOP_RIGHT);
         hb2.getChildren().add(b2);
-        gd.add(hb2, 2, 4);
+        gd.add(hb2, 3, 1);
+        Button b3 = new Button("Draw Map");
+        b3.setOnAction(event -> {
+            try {
+                drawMap();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        HBox hb3 = new HBox(10);
+        hb3.setAlignment(Pos.TOP_LEFT);
+        hb3.getChildren().add(b3);
+        gd.add(hb3, 1, 1);
+
+        HBox hb4 = new HBox();
+        hb4.setAlignment(Pos.TOP_LEFT);
+        logView.setText(logView.getText() + " at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        hb4.getChildren().add(logView);
+        gd.add(hb4, 0, 2, 4, 4);
 
         textArea = new TextArea();
         textArea.setEditable(false);
         textArea.setVisible(false);
         gd.add(textArea, 1, 2);
 
-
-
         textArea.setText("Reock:\t\t\t" + Reock + "\nHarris:\t\t\t" + Harr + "\nConvex Hull:\t\t" + Convex 
                 + "\nPolsby-Popper:\t" + PolsbyP + "\nSchwartzberg:\t\t" + Schwartz);
 
-        b2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent arg0) {
-                textArea.setVisible(true);
-                File file = null;
-                while(file == null) {
-                    file = JFileDataStoreChooser.showOpenFile("shp", null);
-                }
-
-                try {
-                    FileDataStore store = FileDataStoreFinder.getDataStore(file);
-                    featureSource = store.getFeatureSource();
-                    SimpleFeatureCollection collection = featureSource.getFeatures();
-
-                    GeometryDescriptor geomDesc = featureSource.getSchema().getGeometryDescriptor();
-                    geometryAttributeName = geomDesc.getLocalName();
-                    
-                    SimpleFeatureIterator iter = collection.features();
-
-
-                    MapContent map = new MapContent();
-                    map.setTitle("Professor Komo");
-                    Layer layer = new FeatureLayer(featureSource, createDefaultStyle());
-                    map.addLayer(layer);
-                    mapFrame = new JMapFrame(map);
-                    mapFrame.enableToolBar(true);
-                    mapFrame.enableStatusBar(true);
-
-                    /*
-                     * Before making the map frame visible we add a new button to its
-                     * toolbar for our custom feature selection tool
-                     */
-                    JToolBar toolBar = mapFrame.getToolBar();
-                    JButton btn = new JButton("Select");
-                    toolBar.addSeparator();
-                    toolBar.add(btn);
-
-
-                    btn.addActionListener(e -> mapFrame.getMapPane().setCursorTool(
-                            new CursorTool() {
-
-                                @Override
-                                public void onMouseClicked(MapMouseEvent ev) {
-                                    selectFeatures(ev);
-                                }
-                            }));
-                    mapFrame.setSize(500, 500);
-                    mapFrame.setVisible(true);
-                    
-                } catch(IOException e) {
-
-                }
-
-
-            }
-        });
-
-        ps.setScene(new Scene(gd, 500, 500));
+        ps.setScene(new Scene(gd, 1200, 500));
+        gd.getStylesheets().add(MainUI.class.getResource("main.css").toExternalForm());
         ps.show();
     }
 
@@ -399,4 +407,130 @@ public class MainUI extends Application
         return rule;
     }
 
+
+    private void calculate(){
+        File map = this.getRawMap();
+        if (map == null) {
+            logView.setText(logView.getText() + "\nsad (no map found). please load a map.");
+        }
+        logView.setText(logView.getText() + "\nseeds implementing. Please be patient.");
+    }
+
+    private void loadMap() {
+       /* File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            logView.setText(logView.getText() + "\nSomething had a sad. plz go away and try later.");
+        }
+        if (!file.getName().endsWith(".shp")) { // Checking magic numbers is for quiche eaters
+            logView.setText(logView.getText() + "\nError! " + file.getName() + " is not a shapefile. Please try again.");
+        } else {
+            logView.setText(logView.getText() + "\nSuccess! Loaded " + file.getName() + ".");
+            setRawMap(file);
+        }*/
+
+        textArea.setVisible(true);
+        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        if (file == null) {
+            logView.setText(logView.getText() + "\nSomething had a sad. plz go away and try later.");
+            return;
+        }
+        if (!file.getName().endsWith(".shp")) { // Checking magic numbers is for quiche eaters
+            logView.setText(logView.getText() + "\nError! " + file.getName() + " is not a shapefile. Please try again.");
+            return;
+        } else {
+            logView.setText(logView.getText() + "\nSuccess! Loaded " + file.getName() + ".");
+            setRawMap(file);
+        }
+
+        try {
+            FileDataStore store = FileDataStoreFinder.getDataStore(file);
+            featureSource = store.getFeatureSource();
+            SimpleFeatureCollection collection = featureSource.getFeatures();
+
+            GeometryDescriptor geomDesc = featureSource.getSchema().getGeometryDescriptor();
+            geometryAttributeName = geomDesc.getLocalName();
+
+            SimpleFeatureIterator iter = collection.features();
+
+            MapContent map = new MapContent();
+            map.setTitle("Professor Komo");
+            Layer layer = new FeatureLayer(featureSource, createDefaultStyle());
+            map.addLayer(layer);
+            setMc(map);
+
+        } catch(IOException e) {
+
+        }
+
+    }
+
+    private void drawMap() throws IOException {
+       /* File map = this.getRawMap();
+        if (map == null) {
+            logView.setText(logView.getText() + "\nSad (no map found). please load a map.");
+            return;
+        }
+        logView.setText(logView.getText() + "\nDrawing " + map.getName() + "...");
+        FileDataStore store = FileDataStoreFinder.getDataStore(map);
+        SimpleFeatureSource featureSource = store.getFeatureSource();
+
+        // Create a map content and add our shapefile to it
+        MapContent mapC = new MapContent();
+        mapC.setTitle("Prof. Komo's Map Content");*/
+
+        MapContent mapC =getMc();
+        mapFrame = new JMapFrame(mapC);
+        mapFrame.enableToolBar(true);
+        mapFrame.enableStatusBar(true);
+        mapFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        /*
+         * Before making the map frame visible we add a new button to its
+         * toolbar for our custom feature selection tool
+         */
+        JToolBar toolBar = mapFrame.getToolBar();
+        JButton btn = new JButton("Select");
+        toolBar.addSeparator();
+        toolBar.add(btn);
+
+
+        btn.addActionListener(e -> mapFrame.getMapPane().setCursorTool(
+                new CursorTool() {
+
+                    @Override
+                    public void onMouseClicked(MapMouseEvent ev) {
+                        selectFeatures(ev);
+                    }
+                }));
+        mapFrame.setSize(500, 500);
+        mapFrame.setVisible(true);
+
+        // Now display the map
+        //JMapFrame mf = new JMapFrame(mapC);
+
+        // Reimplement showMap() because it's static and dumb so setDefaultCloseOperation doesn't work
+        /*mf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        mf.enableStatusBar(true);
+        mf.enableToolBar(true);
+        mf.initComponents();
+        mf.setSize(800, 600);
+        mf.setVisible(true);*/ // Sometimes the map crashes if you drag too fast. Don't drag too fast.
+        logView.setText(logView.getText() + "\nDraw complete.");
+    }
+
+    private File getRawMap() {
+        return rawMap;
+    }
+
+    private void setRawMap(File rawMap) {
+        this.rawMap = rawMap;
+    }
+
+    public MapContent getMc() {
+        return mc;
+    }
+
+    public void setMc(MapContent mc) {
+        this.mc = mc;
+    }
 }
